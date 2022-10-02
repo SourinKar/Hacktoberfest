b4a92baf072e0753d33d7024d7be51350eedd3d9
SourinKar <102315903+SourinKar@users.noreply.github.com>
2022-10-02 22:22:43 +0530

    Create starpattern_arrow.py

---
+++ 2022/starpattern_arrow.py
@@ -0,0 +1,21 @@
+def printPyramid(size):
+    x = int(size/2)
+    y = 1
+    for i in range(size):
+        printLine(x, y)
+        if i < int(size/2):
+            x += 1
+            y += 1
+        else:
+            x -= 1
+            y -= 1
+
+def printLine(x, y):
+    for i in range(x + 1):
+        print(" ", end = "")
+    for i in range(y * 2-1):
+        print("*", end = "")
+    print()
+    
+    
+printPyramid(9)
